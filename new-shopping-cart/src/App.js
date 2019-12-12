b82cd042c765fcf71c4943de9e520a8510d10904
ahkaneti <aaronkaneti2020@u.northwestern.edu>
2019-12-12 01:40:07 -0600

    Deployed firebase

--- new-shopping-cart/src/App.js
+++ new-shopping-cart/src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTOEmS_C2198-Zgxi1ME8nEmnVPkVC2v0",
+  authDomain: "shoppingcart-dd86e.firebaseapp.com",
+  databaseURL: "https://shoppingcart-dd86e.firebaseio.com",
+  projectId: "shoppingcart-dd86e",
+  storageBucket: "shoppingcart-dd86e.appspot.com",
+  messagingSenderId: "211645572432",
+  appId: "1:211645572432:web:613ea9c1dc6e9c87537855",
+  measurementId: "G-98T1J5168S"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
